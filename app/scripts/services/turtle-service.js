f78649bf3034ab82ae13f6ce2930bcc3d38af1a7
Rachel Wolthuis <rachel.wolthuis@gmail.com>
2014-04-26 10:56:34 -0600

    adds services

---
+++ app/scripts/services/turtle-service.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('tmntappApp')
+  .service('TurtleService', function TurtleService($q, $http) {
+  	return {
+  		getTeams: function() {
+  		  var deferred = $q.defer();
+  	
+  		  $http({method: 'GET', url: 'http://tmnt.devmounta.in/team'}).success(function(data) {
+  		      deferred.resolve(data);
+  		    }
+  		  );
+  		  return deferred.promise;
+  		}}
+  });
